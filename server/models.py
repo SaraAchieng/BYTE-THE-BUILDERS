67202328f44a59042e6ad336c5b7a6b28c2297eb
Sara Achieng <achieng997@gmail.com>
2024-10-15 15:43:21 +0300

    Added Client model

---
+++ server/models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+# contains definitions of tables and associated schema constructs
+metadata = MetaData()
+
+# create the Flask SQLAlchemy extension
+db = SQLAlchemy(metadata=metadata)
+
+# define a model class by inheriting from db.Model.
+class Client:
+    __tablename__ = 'clients'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    phone_number = db.Column(db.String(20))
+    
+
+    
