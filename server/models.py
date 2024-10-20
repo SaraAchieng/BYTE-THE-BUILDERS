2759c70c142eac5e9c8003316c642016b7d0148c
methodius pkemoi <pkemoimethodius@gmail.com>
2024-10-20 21:50:29 +0300

    Adding some routes

--- server/models.py
+++ server/models.py
@@ -51,6 +51,7 @@ class Project(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String, nullable=True)
     client_id = db.Column(db.Integer, db.ForeignKey('clients.id'), nullable=False)
     start_date = db.Column(db.Date, nullable=False)
     end_date = db.Column(db.Date, nullable=True)  # Can be NULL for ongoing projects
@@ -66,11 +67,11 @@ class Material(db.Model):
     __tablename__ = 'materials'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False) 
-    unit = db.Column(db.String(50), nullable=False) 
-    unit_price = db.Column(db.Float, nullable=False)  
-    quantity = db.Column(db.Float, nullable=False)  
-    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'))  
+    name = db.Column(db.String(100), nullable=False)
+    unit = db.Column(db.String(50), nullable=False)
+    unit_price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Float, nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'))
 
     def __repr__(self):
         return f"Material('{self.name}', '{self.unit_price}', '{self.quantity}')"
@@ -80,7 +81,8 @@ class Equipment(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)  # Added 'quantity' column
+    quantity = db.Column(db.Integer, nullable=False) # Added 'quantity' column
+    unit_price = db.Column(db.Float, nullable=True)#May change here
     cost = db.Column(db.Float, nullable=False)
     rental_price = db.Column(db.Float, nullable=True)  # Can be NULL
     purchase_date = db.Column(db.Date, nullable=False)
